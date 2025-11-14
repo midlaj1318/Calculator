ba80b24318805fecd832ff9cb6cd832f966d3d99
midlaj1318 <nkmidlaj1318@gmail.com>
2025-11-14 10:30:16 +0530

    Initial commit - script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+const resultInput = document.getElementById('result');
+let currentInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    resultInput.value = currentInput;
+}
+
+function appendOperator(operator) {
+    if (currentInput === '') return;
+    const lastChar = currentInput[currentInput.length - 1];
+    if (['+', '-', '*', '/'].includes(lastChar)) {
+        currentInput = currentInput.slice(0, -1) + operator;
+    } else {
+        currentInput += operator;
+    }
+    resultInput.value = currentInput;
+}
+
+function clearResult() {
+    currentInput = '';
+    resultInput.value = '';
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    resultInput.value = currentInput;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(currentInput);
+        resultInput.value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        resultInput.value = 'Error';
+        currentInput = '';
+    }
+}
